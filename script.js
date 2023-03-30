9628ab5a938ff4405bac6c24dd3e7cf71d782387
kolechr <kole.chaicharee@gmail.com>
2023-03-30 13:50:01 +0900

    Add dark theme

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.getElementById('search-icon').addEventListener('click', () => {
 
 // Resposive scrolling
 // document.querySelector('.content')
+
+// Dark mode
+document.addEventListener('DOMContentLoaded', (e) => {
+    document.documentElement.setAttribute('data-theme', 'light');
+
+    const themeButton = document.getElementById('theme-button');
+
+    themeButton.onclick = () => {
+        // Change theme
+        let currentTheme = document.documentElement.getAttribute('data-theme');
+        let switchTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute("data-theme", switchTheme);
+
+        //Change icon
+        if (currentTheme === 'light') {
+            themeButton.classList.remove('fa-moon');
+            themeButton.classList.add('fa-lightbulb');
+        }
+        else {
+            themeButton.classList.remove('fa-lightbulb');
+            themeButton.classList.add('fa-moon');
+        }
+    }
+});
